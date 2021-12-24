56e8c5c17a52ce01be8b690a693be6e341584ead
Nurdin <ulanovnurdin@gmail.com>
2021-12-25 00:38:08 +0600

    added exceptionHandler, and deploy on server

--- src/main/java/kg/it/academy/OnlineBank/util/ApiExceptionHandler.java
+++ src/main/java/kg/it/academy/OnlineBank/util/ApiExceptionHandler.java
@@ -3,6 +3,7 @@ package kg.it.academy.OnlineBank.util;
 import kg.it.academy.OnlineBank.exceptions.ApiException;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,10 +13,10 @@ import java.util.Map;
 @ControllerAdvice
 public class ApiExceptionHandler {
     @ExceptionHandler(value = {ApiException.class})
-    public Map<HttpStatus, String> handleFailException(ApiException apiException) {
+    public ResponseEntity<Map<HttpStatus, String>> handleFailException(ApiException apiException) {
         Map<HttpStatus, String> exceptionMessage = new HashMap<>();
         exceptionMessage.put(apiException.getHttpStatus(), apiException.getMessage());
 
-        return exceptionMessage;
+        return new ResponseEntity<>(exceptionMessage, apiException.getHttpStatus());
     }
 }
